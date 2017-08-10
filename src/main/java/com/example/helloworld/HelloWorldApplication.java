865f0e343da2b4f962552e4302331db918771d45
Tomasz Badelek <tbadelek@gmail.com>
2017-08-10 03:41:44 +0200

    1st

---
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+
+	}
+}
